c65433a47ccb8b7b183e5489fc644a22ff1400dc
Rupam Das <rejisterjack@gmail.com>
2025-06-20 21:00:41 +0530

    feat: testing socket

--- petclient/src/App.tsx
+++ petclient/src/App.tsx
@@ -1,4 +1,15 @@
+import { useEffect } from 'react'
+import { createSocketConnection } from './utils/socket'
+
 const App = () => {
+  useEffect(() => {
+    const socket = createSocketConnection()
+    socket.emit('join', { senderId: '111', receiverId: '222' })
+    return () => {
+      socket.disconnect()
+    }
+  }, [])
+
   return <div>App</div>
 }
 
